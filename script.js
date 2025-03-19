1f87cc5ed4a145e1f2962c59b949f743415888e9
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2025-03-19 12:14:28 -0300

    a

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// variavel = elemento "shit"
 var dropdown = document.getElementById('shit');
 function menuabrir() {
-    
+     // se o elemento com o nome "shit" estiver o oposto de visivel ele deixa visivel 
    if (dropdown.style.visibility !='visible'){
         dropdown.style.visibility = 'visible';
 
    }
+//    senão colapsar
    else {
     dropdown.style.visibility = 'collapse';
-     console.log(dropdown.style.display);
+   
 }
    }
    
